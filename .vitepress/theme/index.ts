1c44dc15e8018e48fe46514ca040397f3b5aee31
Fisherd99 <fisherd99@qq.com>
2025-10-03 06:56:18 +0800

    enable zomm in for figure

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,27 @@
 import DefaultTheme from 'vitepress/theme'
 import MyLayout from './MyLayout.vue'
 import './custom.css';
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+
 export default {
   extends: DefaultTheme,
   // 使用注入插槽的包装组件覆盖 Layout
-  Layout: MyLayout
+  Layout: MyLayout,
+
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // 为所有图片增加缩放功能
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  }
 }
