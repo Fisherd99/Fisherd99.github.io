ad0a7b389fd992617c78aeddf9fd883e8df3dbfb
Fisherd99 <fisherd99@qq.com>
2025-10-03 04:16:51 +0800

    add comment by giscus

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
+import Giscus from "@giscus/vue";
 
 const { Layout } = DefaultTheme
 </script>
@@ -8,6 +9,24 @@ const { Layout } = DefaultTheme
   <Layout>
     <template #aside-outline-before>
       导航
+    </template>
+
+    <template #doc-after>
+      <div style="margin-top: 24px">
+        <Giscus
+          repo="Fisherd99/Fisherd99.github.io"
+          repo-id="R_kgDOJmvFtA"
+          category="Announcements"
+          category-id="DIC_kwDOJmvFtM4CwLXf"
+          mapping="pathname"
+          strict="0"
+          reactions-enabled="1"
+          emit-metadata="0"
+          input-position="top"
+          theme="preferred_color_scheme"
+          lang="zh-CN"
+        />
+      </div>
     </template>
   </Layout>
 </template>
